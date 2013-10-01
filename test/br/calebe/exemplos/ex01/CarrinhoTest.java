a8ceb91492eedb415e670f0a3c0a95ebd39b04f7
PauloPereira <PauloPereira@PauloPereira-PC>
2013-10-01 17:05:32 -0300

    Feito a Fase 3 projeto carrinho

--- test/br/calebe/exemplos/ex01/CarrinhoTest.java
+++ test/br/calebe/exemplos/ex01/CarrinhoTest.java
@@ -1,5 +1,7 @@
 package br.calebe.exemplos.ex01;
 
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.assertArrayEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -47,5 +49,68 @@ public class CarrinhoTest {
         Produto copia = new Produto("Java em 24 horas", 50.00);
         original = carrinho.menorProduto();
         assertArrayEquals(new Object[]{original}, new Object[]{copia});
+    }
+
+    //adicionar outros tipos de produtos ao carrinho
+    @Test
+    public void adcinonarNovosTiposProdutos() {
+        String tipo = "Eletrônico";
+        assertArrayEquals(new Object[]{tipo}, new Object[]{"Eletrônico"});
+    }
+    
+    //listar todos os produtos do carrinho
+    @Test
+    public void listarProdutos(){
+        
+        List<String> lista = new ArrayList<>();
+        
+        lista.add("CD Victor Léo");
+        lista.add("Bicicleta");
+        lista.add("Geladeira");
+        
+        assertArrayEquals(new Object[]{lista.get(0)}, new Object[]{"CD Victor Léo"});
+        assertArrayEquals(new Object[]{lista.get(1)}, new Object[]{"Bicicleta"});
+        assertArrayEquals(new Object[]{lista.get(2)}, new Object[]{"Geladeira"});
+        
+    }
+    
+   //remover um produto do carrinho
+    @Test
+    public void removerProduto(){
+        
+        List<String> lista = new ArrayList<>();
+        
+       
+        lista.add("CD Victor Léo");
+        lista.add("Bicicleta");
+        lista.add("Geladeira");
+       
+        
+        lista.remove("Sabonete");
+        
+        
+        assertArrayEquals(new Object[]{lista.size()}, new Object[]{2});
+        
+    }
+    
+    //Calcular o total que deve ser pago pelos produtos no carrinho
+    @Test
+    public void totalPago(){
+        
+        List<Double> lista = new ArrayList<>();
+        
+        double total = 0.0;
+        
+        lista.add(40.00);
+        lista.add(40.00);
+        lista.add(50.00);
+        
+        //calcula o total de valores dentro da lista
+        for(Double x : lista){
+            total = total + x.doubleValue();
+        }
+        
+        assertArrayEquals(new Object[]{total}, new Object[]{190.00});
+        
     }
 }
